0b8b0dfba392590af9f61eb38dde75ff1494b39e
devilayan07 <samaddarayan8@gmail.com>
2025-02-14 18:02:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Hospital from './CMS/HospitalData/Hospital'
+
+function App() {
+
+  return (
+    <>
+    <Hospital/>
+    </>
+  )
+}
+
+export default App
---
+++ src/CMS/HospitalData/Hospital.jsx
@@ -0,0 +1,496 @@
+import React from 'react'
+import Invoice from '../Invoice/Invoice'
+
+
+const userData={
+    
+        "invoiceDetails": {
+            "displayId": "KC-BU-2025-IV1000039",
+            "invoiceDate": "2025-02-05T09:28:47.311Z",
+            "_id": null
+        },
+        "supplierDetails": {
+            "name": "Branch two",
+            "gstin": "22ABCDE1234F1Z5",
+            "address": "dhhd Asansol West Bengal India",
+            "email": "kasifbu2bnch2@yopmail.com",
+            "phone": "9999999102",
+            "_id": "67820e34a840f3a7bf1a312d",
+            "displayId": "KC-BU-2024-BR1000002"
+        },
+        "recipientDetails": {
+            "name": "Patient 3",
+            "lastName": "no",
+            "email": "patien3@gmail.com",
+            "phone": "4545454545",
+            "_id": "678b23dfd6ef2ca20e1f9a6b",
+            "displayId": "KC-BU-2024-PT1000005"
+        },
+        "taxDetails": {
+            "totalTaxableValue": 1582,
+            "totalCGST": 0,
+            "totalSGST": 0,
+            "totalIGST": 0,
+            "totalCess": 0
+        },
+        "totalAmount": {
+            "totalValue": 1582,
+            "inWords": "1582.00 Only"
+        },
+        "netAmount": {
+            "totalValue": 0,
+            "inWords": "Zero Only"
+        },
+        "_id": "67a32f52074ac38a6f71a925",
+        "reverseCharge": false,
+        "itemKart": [
+            {
+                "itemName": "assas",
+                "hsnSacCode": "1001",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 582,
+                "taxableValue": 582,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 582,
+                "_id": "67a32f5a074ac38a6f71a928"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+            {
+                "itemName": "Service 2",
+                "hsnSacCode": "25847",
+                "quantity": 1,
+                "discount": 0,
+                "unitPrice": 1000,
+                "taxableValue": 1000,
+                "cgst": 0,
+                "sgst": 0,
+                "igst": 0,
+                "total": 1000,
+                "_id": "67a33865f4734190e4271e60"
+            },
+
+
+
+
+        ],
+        "discount": 0,
+        "signature": null,
+        "createdBy": null,
+        "createdAt": "2025-02-05T09:28:50.512Z",
+        "__v": 2,
+        "index": 1
+    
+}
+function Hospital() {
+  return (
+    <>
+      <Invoice userData={userData}/>
+    </>
+  )
+}
+
+export default Hospital
